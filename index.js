fa7d74beb36097bf5a9e65045b7fcaf07c0de530
KingBrink <kyle.brink1602@gmail.com>
2024-08-06 11:25:20 +0200

    yeah

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-//
+import express from 'express';
+import path from 'path'
+import {connection} from './config/index.js'
+// create an express app
+const app = express()
+const port = +process.env.PORT || 4000
+const router = express.Router()
+// middleware
+app.use(router, express.static('./static'))
+// endpoint
+router.get('^/$|/eShop', (req, res) => {
+    res.status(200).sendFile(path.resolve('./static/html/index.html'))
+})
+app.listen(port, () => {
+    console.log('Server is running on port: ' + port)
+})
